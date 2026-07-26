5f2a0cfa19b4cc148dde09295b674a371d8903e9
Furgan Gurbanli <f.qurbanli.2006@gmail.com>
2026-07-26 22:43:47 +0400

    struct added

--- main.c
+++ main.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+typedef struct Users {
+    char name[100];
+    int age;
+    struct Node* next;
+}User;
 
 int main(void) {
-    printf("Hello, World!\n");
+
+
     return 0;
 }
